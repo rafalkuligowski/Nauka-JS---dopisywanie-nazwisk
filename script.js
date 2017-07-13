2e50b1f04aacf5496c8e88168738b889f332513b
rafalkuligowski <rafal.kuligowski87@gmail.com>
2017-07-13 23:01:16 +0200

    script

---
+++ script.js
@@ -0,0 +1,11 @@
+var addBtn = document.querySelector(".btn");
+addBtn.addEventListener("click", function ()
+{
+	var li = document.createElement("li");
+	var list = document.querySelector (".list");
+	var input = document.querySelector (".text");
+	li.innerHTML = input.value;
+	list.appendChild(li);
+	
+},false )
+
